17cd2fcf9cff94a419e12d9f0770d7ee70e8ed24
Asif Shariar <shariarasif0@gmail.com>
2025-09-07 22:32:52 +0600

    Uploaded

--- ColorPicker/main.js
+++ ColorPicker/main.js
@@ -6,24 +6,19 @@ const msg = document.getElementById('msg');
 let currentHex = colorInput.value;
 let currentRGB = HextoRGB(currentHex);
 
-
 colorInput.addEventListener('input', () => {
     currentHex = colorInput.value;
     currentRGB = HextoRGB(currentHex);
 
     hex.textContent = `HEX: ${currentHex}`;
-    rgb.textContent = `RGB: ${currentRGB}`
-
+    rgb.textContent = `RGB: ${currentRGB}`;
     msg.textContent = '';
-
 });
 
 function HextoRGB(hex) {
-
     const r = parseInt(hex.substr(1,2),16);
-    const g = parseInt(hex.substr(3,2),16)
+    const g = parseInt(hex.substr(3,2),16);
     const b = parseInt(hex.substr(5,2),16);
-
     return `rgb(${r},${g},${b})`;
 }
 
